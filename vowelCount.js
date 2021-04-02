1dc66a8322149af7c8b629d3a7c05328e534d7e2
Ramon Ariel Leon Flores <arielleon24@gmail.com>
2021-04-02 16:46:52 -0400

    added the isNaN filter

--- vowelCount.js
+++ vowelCount.js
@@ -29,11 +29,22 @@ const assertArraysEqual = function (array1, array2) {
 };
 
 function getCount(str) {
+  const obj = {a: 0, i: 0, u:0, e:0, o: 0 }
   let vowelsCount = 0;
   
-  // enter your majic here
+  for (let vowel of str) {
+    obj[vowel] += 1;
+  }
+  console.log(obj)
+
+  for (let key in obj) {
+    if (!isNaN(obj[key])) {
+      vowelsCount += obj[key]
+    }
+  }
   
   return vowelsCount;
 }
 
 assertEqual(getCount("aiueo"), 5)
+assertEqual(getCount("abracadabra"), 5)
