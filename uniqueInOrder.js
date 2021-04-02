ff4753fb471959308fb577d801970349fd4d947c
Ramon Ariel Leon Flores <arielleon24@gmail.com>
2021-04-02 16:09:06 -0400

    manual assertions

--- uniqueInOrder.js
+++ uniqueInOrder.js
@@ -1,34 +1,34 @@
-// const assertEqual = function(actual, expected) {
-//   if (actual === expected) {
-//     console.log(`✅ Assertion Passed ✅: ${actual} === ${expected}`);
-//     return true;
-//   } else {
-//     console.log(`❌ Assertion Failed ❌: ${actual} !== ${expected}`);
-//     return false;
-//   }
-// };
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed ✅: ${actual} === ${expected}`);
+    return true;
+  } else {
+    console.log(`❌ Assertion Failed ❌: ${actual} !== ${expected}`);
+    return false;
+  }
+};
 
-// const eqArrays = function (array1, array2) {
-//   if (array1.length !== array2.length) {
-//     return false;
-//   } else {
-//   }
-//   for (let i = 0; i < array1.length; i++) {
-//     if (array1[i] !== array2[i]) {
-//       return false;
-//     }
-//   }
-//   return true;
-// };
+const eqArrays = function (array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  } else {
+  }
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
 
-// const assertArraysEqual = function (array1, array2) {
-//   let results = eqArrays(array1, array2)
-//   if (!results) {
-//     console.log(`😡😡😡 Assertion Failed: ${array1} !== ${array2}`);
-//   } else {
-//     console.log(`😃😃😃 Assertion Passed: ${array1} === ${array2}`);
-//   }
-// };
+const assertArraysEqual = function (array1, array2) {
+  let results = eqArrays(array1, array2)
+  if (!results) {
+    console.log(`😡😡😡 Assertion Failed: ${array1} !== ${array2}`);
+  } else {
+    console.log(`😃😃😃 Assertion Passed: ${array1} === ${array2}`);
+  }
+};
 
 
 //// Code above are assertions 
@@ -40,10 +40,8 @@ let uniqueInOrder=function(iterable){
   for(let element of iterable) {
     console.log(element)
     if (element === iterable[index]) {
-      console.log("matches")
       index ++
     } else {
-      console.log("doesn't")
       array.push(element)
       index ++
     }
@@ -53,6 +51,6 @@ let uniqueInOrder=function(iterable){
   
   }
 
-  Test.assertSimilar(uniqueInOrder('AAAABBBCCDAABBB'), ['A','B','C','D','A','B'])
-  Test.assertSimilar(uniqueInOrder('ABBCcAD'), ['A', 'B', 'C', 'c', 'A', 'D'])
-  Test.assertSimilar(uniqueInOrder([1,2,2,3,3]) , [1,2,3])
+assertArraysEqual(uniqueInOrder('AAAABBBCCDAABBB'), ['A','B','C','D','A','B'])
+assertArraysEqual(uniqueInOrder('ABBCcAD'), ['A', 'B', 'C', 'c', 'A', 'D'])
+assertArraysEqual(uniqueInOrder([1,2,2,3,3]) , [1,2,3])
