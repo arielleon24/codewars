48a14b4905272973dc41d31f24daaab08a6d309d
Ramon Ariel Leon Flores <arielleon24@gmail.com>
2021-04-03 10:20:36 -0400

    initial setup

---
+++ IQtest.js
@@ -0,0 +1,43 @@
+//assertions functions 
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed ✅: ${actual} === ${expected}`);
+    return true;
+  } else {
+    console.log(`❌ Assertion Failed ❌: ${actual} !== ${expected}`);
+    return false;
+  }
+};
+const eqArrays = function (array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  } else {
+  }
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+const assertArraysEqual = function (array1, array2) {
+  let results = eqArrays(array1, array2)
+  if (!results) {
+    console.log(`😡😡😡 Assertion Failed: ${array1} !== ${array2}`);
+  } else {
+    console.log(`😃😃😃 Assertion Passed: ${array1} === ${array2}`);
+  }
+};
+
+
+///Kata
+
+
+
+
+
+
+//TEST 
+
+assertEqual(iqTest("2 4 7 8 10"),3);
+assertEqual(iqTest("1 2 2"), 1);
