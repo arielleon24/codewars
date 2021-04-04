7def2a49aee87a7a3c4d2c3d53716c9f3bbaaaa1
Ramon Ariel Leon Flores <arielleon24@gmail.com>
2021-04-04 06:36:40 -0400

    created js modules for assertions in order to clean up code

---
+++ assertEqual.js
@@ -0,0 +1,11 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed ✅: ${actual} === ${expected}`);
+    return true;
+  } else {
+    console.log(`❌ Assertion Failed ❌: ${actual} !== ${expected}`);
+    return false;
+  }
+};
+
+module.exports = assertEqual;
