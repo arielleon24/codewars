2b374606c10a4dd0f0d9f5a4daf4016da63b9312
Ramon Ariel Leon Flores <arielleon24@gmail.com>
2021-04-06 10:24:00 -0400

    fixed typo

--- multiples3And5.js
+++ multiples3And5.js
@@ -8,15 +8,19 @@ function getSum(total, num) {
 function solution(number){
   const array = [];
   for(i = number - 1; i > 0; i--) {
-    console.log(i)
     if(i % 3 === 0) {
       array.push(i)
     } else if (i % 5=== 0) {
       array.push(i)
     }
   }
+  console.log(array)
+  if (array.length > 0) {
+    
   return array.reduce(getSum)
-}
-
+  } else {
+    return 0
+  }
+}  
 
 assertEqual(solution(10), 23)
