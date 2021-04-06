b0ededecd3760d36d8b2ebd010a96768823bcf68
Ramon Ariel Leon Flores <arielleon24@gmail.com>
2021-04-05 23:16:28 -0400

    solved

--- multiples3And5.js
+++ multiples3And5.js
@@ -1,11 +1,22 @@
 /// Assertions
 const assertEqual = require("./assertEqual")
 
+function getSum(total, num) {
+  return total + Math.round(num);
+}
+
 function solution(number){
-  for(i = number; i > 0; i--) {
-    console.log(number)
+  const array = [];
+  for(i = number - 1; i > 0; i--) {
+    console.log(i)
+    if(i % 3 === 0) {
+      array.push(i)
+    } else if (i % 5=== 0) {
+      array.push(i)
+    }
   }
+  return array.reduce(getSum)
 }
 
 
-assertEqual(solution(10,23))
+assertEqual(solution(10), 23)
