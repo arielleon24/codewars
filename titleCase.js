fcd549c8e006e65b80d70631187bfb9cfa0be3ac
Ramon Ariel Leon Flores <arielleon24@gmail.com>
2021-04-21 23:03:39 -0400

    initial setup

---
+++ titleCase.js
@@ -0,0 +1,8 @@
+
+
+
+// Test cases
+assertEquals(titleCase(''), '')
+assertEquals(titleCase('a clash of KINGS', 'a an the of'), 'A Clash of Kings')
+assertEquals(titleCase('THE WIND IN THE WILLOWS', 'The In'), 'The Wind in the Willows')
+assertEquals(titleCase('the quick brown fox'), 'The Quick Brown Fox')
