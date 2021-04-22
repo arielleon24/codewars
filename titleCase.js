3d459409c05a688df6234a3fa14ff9b51c455f95
Ramon Ariel Leon Flores <arielleon24@gmail.com>
2021-04-21 23:41:41 -0400

    1 assertion missing

--- titleCase.js
+++ titleCase.js
@@ -1,6 +1,11 @@
 /// Assertions
 const assertEquals = require("./assertEqual")
 
+const modify = (word) =>{
+  let upper = word[0].toUpperCase()
+  let rest = word.substring(1)
+  return upper + rest
+}
 
 function titleCase(title, minorWords) {
   const array = []
@@ -9,23 +14,30 @@ function titleCase(title, minorWords) {
   } else if (!minorWords) {
     const titleWords = title.split(" ")
     for (let words of titleWords) {
-      let upper = words[0].toUpperCase()
-      const rest = words.substring(1)
-      array.push(upper + rest)
+      array.push(modify(words))
     }
     return array.join(' ')
 
   } else {
-    const titleWords = title.split(" ")
-    console.log(minorWords) 
-    const exception = minorWords.split(" ")
-    console.log(titleWords)
+    const titleWords = title.toLowerCase().split(" ")
+    const exception = minorWords.toLowerCase().split(" ")
+    titleWords.forEach(word => {
+      if(word === titleWords[0]){
+        array.push(modify(word))
+      } else if (word !== titleWords[0] && !exception.includes(word)) {
+        array.push(modify(word))
+      } else if (exception.includes(word)) {
+        array.push(word)
+      }
+    });
+    console.log(array.join(' '))
+    return array.join(' ')
   }
   
 }
 
 // Test cases
 // assertEquals(titleCase(''), '')
-// assertEquals(titleCase('a clash of KINGS', 'a an the of'), 'A Clash of Kings')
-// assertEquals(titleCase('THE WIND IN THE WILLOWS', 'The In'), 'The Wind in the Willows')
-assertEquals(titleCase('the quick brown fox'), 'The Quick Brown Fox')
+assertEquals(titleCase('a clash of KINGS', 'a an the of'), 'A Clash of Kings')
+assertEquals(titleCase('THE WIND IN THE WILLOWS', 'The In'), 'The Wind in the Willows')
+// assertEquals(titleCase('the quick brown fox'), 'The Quick Brown Fox')
