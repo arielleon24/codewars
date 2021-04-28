6e19b347174189560fd16509f728e10a14287167
Ramon Ariel Leon Flores <arielleon24@gmail.com>
2021-04-27 23:06:12 -0400

    looping arr

--- moveZeros.js
+++ moveZeros.js
@@ -1,6 +1,13 @@
 /// Assertions
 const assertEqual = require("./assertEqual")
 
+let moveZeros = function (arr) {
+  let newArray = [];
+  for(let i of arr) {
+    
+  }
+}
+
 
 //test cases
 assertEqual(moveZeros([1,2,0,1,0,1,0,3,0,1]), [1, 2, 1, 1, 3, 1, 0, 0, 0, 0])
