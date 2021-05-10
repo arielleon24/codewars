d2d5e62547514c003a879f786afd28d478e6f480
Ramon Ariel Leon Flores <arielleon24@gmail.com>
2021-05-09 21:36:36 -0400

    more assertions

--- phoneParser.js
+++ phoneParser.js
@@ -12,3 +12,6 @@ const phoneParse = (arr) => {
 }
 
 assertEqual(phoneParse([5,1,4,5,3,1,4,7,3,2]), "(514) 531-4732")
+assertEqual(phoneParse([5,1,4,2,3,2,4,2,7,2]), "(514) 232-4272")
+assertEqual(phoneParse([5,1,4,5,3,1,4,7,3,2]), "(450) 333-4455")
+assertEqual(phoneParse([5,1,4,5,3,1,4,7,3,2]), "(819) 529-5532")
