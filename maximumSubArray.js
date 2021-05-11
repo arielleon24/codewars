82a2b828f4413f13644988b2dd3063bc520ae1db
Ramon Ariel Leon Flores <arielleon24@gmail.com>
2021-05-10 22:06:32 -0400

    simple solution

--- maximumSubArray.js
+++ maximumSubArray.js
@@ -1,7 +1,11 @@
 const maxSequence = function(arr){
+  const total = 0
   if(arr.length < 1) {
     return 0
   } else {
-    
+    arr.forEach(x => {
+      total += x
+    });
   }
+  return total
 }
